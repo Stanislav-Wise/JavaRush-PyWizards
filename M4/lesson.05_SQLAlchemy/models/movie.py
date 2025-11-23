dc9115a5420ce2dbc8dc0319d92b728cf71f7109
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-11-23 13:19:31 +0300

    Add lesson.05: SQLAlchemy

---
+++ M4/lesson.05_SQLAlchemy/models/movie.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from db.session import Base
+
+
+class Movie(Base):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    description = Column(Text)
+    genre_id = Column(Integer, ForeignKey('genres.id'))
+
+    # movie.genre.name
+    genre = relationship("Genre", back_populates="movies")
+    reviews = relationship("Review", back_populates="movie")
---
+++ M4/lesson.05_SQLAlchemy/schemas/movie.py
@@ -0,0 +1,93 @@
+from datetime import date
+from pydantic import BaseModel, Field, field_validator  #Старый способ -  @validator
+
+
+class MovieBase(BaseModel):
+
+    title: str = Field(
+        ...,
+        min_length=1,
+        max_length=100,
+        description="Movie Title",
+        examples=['The Matrix', 'Game', 'I am Robot']
+    )
+    year: int = Field(
+        ...,
+        # ge=1850,
+        # le=date.today().year,
+        description="Movie Year",
+        examples=[1999, 2000, 2015],
+    )
+    description: str | None = Field(
+        None,
+        description="Movie Description",
+        examples=['Фантастический фильм про виртуальную реальность'],
+    )
+
+    # @validator - устаревшее
+
+    @field_validator('title')
+    @classmethod # рекомендуется указывать
+    def normalize_title(cls, value: str) -> str:
+        clean_value = value.strip()
+
+        if not clean_value:
+            raise ValueError('Заголовок не может состоять из одних пробелов.')
+
+        return clean_value
+
+
+    @field_validator('year')
+    @classmethod # рекомендуется указывать
+    def check_year(cls, value: str) -> str:
+        current_year = date.today().year
+
+        if value > current_year:
+            raise ValueError('Год не должен быть больше текущего года')
+
+        if value < 1850:
+            raise ValueError('Год не должен быть меньше 1850 года')
+
+        return value
+
+
+class Movie(MovieBase):
+    id: int = Field(
+        ...,
+        description="Movie ID",
+        examples=[1, 2, 3, 4, 5]
+    )
+
+
+movie_list = [
+    Movie(
+        id=1,
+        title="Mатрица",
+        year=1999,
+        description="Movie Description 1",
+    ),
+    Movie(
+        id=2,
+        title="Звездные войны",
+        year=1981,
+        description="Movie Description 2",
+    ),
+    Movie(
+        id=3,
+        title="Игра",
+        year=1999,
+        description="Movie Description 3",
+    ),
+    Movie(
+        id=4,
+        title="Игра престолов",
+        year=2005,
+        description="Movie Description 4",
+    ),
+    Movie(
+        id=5,
+        title="Терминатор",
+        year=1987,
+        description="Movie Description 5",
+    ),
+]
